36a27101dfe47ab8a524597867cbb65b62c45e7e
Nox <leandro.aeschbacher77@gmail.com>
2026-01-27 21:12:11 +0100

    Add multiprocessing.freeze_support() to main.py
    Add multiprocessing freeze support for compatibility.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import multiprocessing
+from epub_builder_gui import main
 from epub_builder_gui import main
 
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()  # <--- Add this line FIRST
     main()
